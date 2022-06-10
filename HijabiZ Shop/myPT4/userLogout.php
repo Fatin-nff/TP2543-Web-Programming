09b3e817bb883632cfc1b9d81bf3800828831317
Fatin-nff <75302275+Fatin-nff@users.noreply.github.com>
2022-06-10 20:32:12 +0800

    Delete HijabiZ Shop directory

--- HijabiZ Shop/myPT4/userLogout.php
+++
@@ -1,9 +0,0 @@
-<?php  
-	require 'database.php';
-	if (!isset($_SESSION['loggedin']))
-		header("LOCATION: userLogin.php"); 
-
-	session_start();  
-	session_destroy();  
-	header("location:userLogin.php");  
-?> 
